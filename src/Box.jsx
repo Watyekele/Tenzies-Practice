b018416e6504a1b3452493046dfbac6aed20fddc
Naluume <watyekeleawalin@gmail.com>
2025-05-08 16:39:04 +0300

    Worked on the randomness of the Dice Values

--- src/Box.jsx
+++ src/Box.jsx
@@ -4,7 +4,29 @@ import DiceComp from "./DiceComp";
 import Header from "./Header";
 
 export default function Box() {
-  const [Dice, setDice] = useState(null);
+  function generateDice(id) {
+    return {
+      id: id,
+      value: Math.floor(Math.random() * 6 + 1),
+      isHeld: false,
+    };
+  }
+
+  function allNewDice() {
+    const Dice = [];
+
+    for (let i = 0; i < 10; i++) {
+      Dice.push(generateDice(i));
+    }
+    return Dice;
+  }
+
+  const [Dice, setDice] = useState(allNewDice());
+
+  const diceElements = Dice.map((Die, id) => (
+    <DiceComp key={Die.id} value={Die.value} status={Die.isHeld} />
+  ));
+
   return (
     <div
       className="border border-8 border-white bg-white
@@ -17,7 +39,9 @@ export default function Box() {
       >
         <Header />
 
-        <DiceComp />
+        <section className="grid grid-cols-5 grid-rows-2 ">
+          {diceElements}
+        </section>
         <button
           className="border 
         border-none mb-2 px-2 rounded-sm bg-blue-300 
--- src/DiceComp.jsx
+++ src/DiceComp.jsx
@@ -1,16 +1,11 @@
-export default function DiceComp() {
+export default function DiceComp(props) {
   return (
-    <div className="flex gap-4 m-2 w-50 flex-wrap">
-      <button className="border border-black px-2 rounded ">1</button>
-      <button className="border border-black px-2 rounded ">1</button>
-      <button className="border border-black px-2 rounded ">1</button>
-      <button className="border border-black px-2 rounded ">1</button>
-      <button className="border border-black px-2 rounded ">1</button>
-      <button className="border border-black px-2 rounded ">1</button>
-      <button className="border border-black px-2 rounded ">1</button>
-      <button className="border border-black px-2 rounded ">1</button>
-      <button className="border border-black px-2 rounded ">1</button>
-      <button className="border border-black px-2 rounded ">1</button>
-    </div>
+    <button
+      className="
+    border border-black 
+    px-2 m-2 rounded "
+    >
+      {props.value}
+    </button>
   );
 }
