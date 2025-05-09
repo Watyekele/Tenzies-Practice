057d91aa7f48f938dde2eabb50578fc75249dc1b
Naluume <watyekeleawalin@gmail.com>
2025-05-09 14:18:20 +0300

    Worked on changing the color of held dice

--- src/Box.jsx
+++ src/Box.jsx
@@ -23,13 +23,36 @@ export default function Box() {
 
   const [Dice, setDice] = useState(allNewDice());
 
+  function Hold(id) {
+    console.log("Holding Dice with id:", id);
+
+    setDice((prevState) =>
+      prevState.map((Die) =>
+        Die.id === id ? { ...Die, isHeld: !Die.isHeld } : Die
+      )
+    );
+  }
+  function rollSingleDie() {
+    setDice((prevState) =>
+      prevState.map((Die) =>
+        Die.isHeld ? Die : { ...Die, value: Math.floor(Math.random() * 6 + 1) }
+      )
+    );
+  }
+
   const diceElements = Dice.map((Die, id) => (
-    <DiceComp key={Die.id} value={Die.value} status={Die.isHeld} />
+    <DiceComp
+      key={Die.id}
+      value={Die.value}
+      status={Die.isHeld}
+      id={Die.id}
+      hold={Hold}
+    />
   ));
 
-  function handleClick() {
-    setDice(allNewDice());
-  }
+  // function handleClick() {
+  //   setDice(allNewDice());
+  // }
 
   return (
     <div
@@ -42,18 +65,18 @@ export default function Box() {
        rounded w-full h-full gap-2"
       >
         <Header />
-
-        <section className="grid grid-cols-5 grid-rows-2 gap-4 ">
-          {diceElements}
-        </section>
-        <button
-          onClick={handleClick}
-          className="border 
-        border-none mb-2 px-2 rounded-sm bg-blue-200 
+        <div className="flex flex-col gap-4 items-center mb-4">
+          <section className="grid grid-cols-5 grid-rows-2 gap-4 ">
+            {diceElements}
+          </section>
+          <button
+            className="w-20
+        border-none  px-2 rounded-sm bg-blue-200 
         shadow-[2px_3px_4px_rgba(0,0,0,0.5)] font-semibold cursor-pointer hover:opacity-50"
-        >
-          Roll Dice
-        </button>
+          >
+            Roll
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/DiceComp.jsx
+++ src/DiceComp.jsx
@@ -1,12 +1,16 @@
 export default function DiceComp(props) {
   return (
     <button
-      className="
+      onClick={() => props.hold(props.id)}
+      className={`
      border-none
     px-2 m-1 rounded text-xl
-     cursor-pointer shadow-[2px_3px_3px_rgba(0,0,0,0.5)]"
+     cursor-pointer shadow-[2px_3px_3px_rgba(0,0,0,0.5)] ${
+       props.status ? "bg-green-300" : "bg-blue-200"
+     }`}
     >
       {props.value}
     </button>
   );
 }
+``;
